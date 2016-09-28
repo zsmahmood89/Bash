56dfc1264234a0b328ba7f7757d0cdc6f869c694
Zuhaib Sheikh Mahmood <mahmoo21@msu.edu>
2016-09-28 13:10:25 -0400

    abbyy script, repeats until maxiterations or until all files done

--- abbyy.sh
+++ abbyy.sh
@@ -1,32 +1,53 @@
 #!/bin/bash
 
+ARG1=${1:-7}
+
 source="/Users/Z/Desktop/temp"
 dest="/Users/Z/Desktop/abby_ocr"
 final="/Users/Z/Desktop/abby_ocr_text"
 ext=".txt"
+killtime=$((ARG1 * 4))
+pdfcount=`ls -1 *.pdf 2>/dev/null | wc -l` #count of pdf files in input dir
 
 cd $source
-for file in *.pdf
+
+while [ $pdfcount != 0]
 	do
-		fraw=$(basename "$file")
-		fname="${fraw%.*}"
-		echo "${file}"
-		mv "${file}" "$dest"
-		counter=0
-		until [ -e "$final/$fname$ext" ]
+		for file in *.pdf
 			do
-				if [ $counter -eq 20 ]
+				fraw=$(basename "$file")
+				fname="${fraw%.*}"
+				scp "${file}" "$dest"
+				counter=0
+				err=0
+				until [ -e "$final/$fname$ext" ]
+					do
+						if [ $counter -eq killtime ]
+							then
+								err=1
+								break
+							else
+								sleep 15s
+								counter=$((counter+1))
+						fi
+					done
+				if [ $err == 0 ]
 					then
-						exit 1
+						echo "${file}"
+						rm "$dest/${file}"
 					else
-						sleep 30s
-						counter=$((counter+1))
+						echo "Will return to ${file}"
 				fi
+				sleep 3s
 			done
-		#rm "$dest/$file"
-		#echo "$fname"
-		#echo "$file"
-		#echo "$final/$fname$ext"
+
+		pdfcount=`ls -1 *.pdf 2>/dev/null | wc -l`
+		if [ $iterations -eq maxit ]
+			then
+				echo "WARNING: Max iterations reached, remaining PDFs were not completed"
+				exit 1
+		fi
 	done
-	sleep 10s
-echo "done"
+	
+echo "All your PDFs are belong to us"
+echo "Complete"
